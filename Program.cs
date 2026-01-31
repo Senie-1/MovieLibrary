ee9fe3639ebd0d13047c4ec603589a41f2851d6d
Виктория Иванова <vi08575866@edu.mon.bg>
2026-01-31 13:15:44 +0200

    add roles

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
+using MovieLibrary.Data.Seeds;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await IdentitySeeder.SeedAsync(services);
+}
 
 if (!app.Environment.IsDevelopment())
 {
@@ -20,6 +31,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
