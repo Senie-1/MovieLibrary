f65bbf2e61c6948cdfc3bbedac277554f8cf97c9
Senie-1 <seniemxmd@gmail.com>
2026-01-31 12:08:02 +0200

    views

--- Controllers/GenresController.cs
+++ Controllers/GenresController.cs
@@ -1,83 +1,39 @@
 ﻿
-    namespace MovieLibrary.Controllers
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieLibrary.Data;
+using MovieLibrary.Models;
+
+namespace MovieLibrary.Controllers
+{
+    public class GenresController : Controller
     {
-        public class GenresController : Controller
+        private readonly ApplicationDbContext _context;
+
+        public GenresController(ApplicationDbContext context)
         {
-            private readonly ApplicationDbContext _context;
+            _context = context;
+        }
 
-            public GenresController(ApplicationDbContext context)
-            {
-                _context = context;
-            }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Genres.ToListAsync());
+        }
 
-            public async Task<IActionResult> Index()
-            {
-                return View(await _context.Genres.ToListAsync());
-            }
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-            public async Task<IActionResult> Details(int id)
-            {
-                var genre = await _context.Genres
-                    .Include(g => g.MovieGenres)
-                    .ThenInclude(mg => mg.Movie)
-                    .FirstOrDefaultAsync(g => g.Id == id);
-
-                if (genre == null)
-                    return NotFound();
-
+        [HttpPost]
+        public async Task<IActionResult> Create(Genre genre)
+        {
+            if (!ModelState.IsValid)
                 return View(genre);
-            }
 
-            public IActionResult Create()
-            {
-                return View();
-            }
-
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> Create(Genre genre)
-            {
-                if (!ModelState.IsValid)
-                    return View(genre);
-
-                _context.Genres.Add(genre);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-
-            public async Task<IActionResult> Edit(int id)
-            {
-                var genre = await _context.Genres.FindAsync(id);
-                if (genre == null)
-                    return NotFound();
-
-                return View(genre);
-            }
-
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> Edit(Genre genre)
-            {
-                if (!ModelState.IsValid)
-                    return View(genre);
-
-                _context.Genres.Update(genre);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-
-            public async Task<IActionResult> Delete(int id)
-            {
-                var genre = await _context.Genres.FindAsync(id);
-                if (genre == null)
-                    return NotFound();
-
-                _context.Genres.Remove(genre);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
+            _context.Genres.Add(genre);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
-
-
-
+}
