0a8e060163d8ee96fd9a1d508b1367a838bc69ce
viki-417 <vuctoriaa738@gmail.com>
2026-02-15 12:26:58 +0200

    update actor controller

--- MovieLibrary.Web/Controllers/ActorController.cs
+++ MovieLibrary.Web/Controllers/ActorController.cs
@@ -1,81 +1,93 @@
-﻿
-using MovieLibrary.Data.Persistance;
-using MovieLibrary.Models.Domain.Entities;
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieLibrary.Business.Services;
+using MovieLibrary.Models.ViewModels.Actors;
 
 namespace MovieLibrary.Controllers
+{
+    public class ActorsController : Controller
     {
-        public class ActorsController : Controller
+        private readonly IActorService _actorService;
+
+        public ActorsController(IActorService actorService)
         {
-            private readonly ApplicationDbContext _context;
+            _actorService = actorService;
+        }
 
-            public ActorsController(ApplicationDbContext context)
-            {
-                _context = context;
-            }
+        // GET: Actors
+        public async Task<IActionResult> Index()
+        {
+            var actors = await _actorService.GetAllAsync();
+            return View(actors);
+        }
 
-            public async Task<IActionResult> Index()
-            {
-                return View(await _context.Actors.ToListAsync());
-            }
+        // GET: Actors/Details/{id}
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var actor = await _actorService.GetByIdAsync(id);
 
-            public async Task<IActionResult> Details(Guid id)
-            {
-                var actor = await _context.Actors.FindAsync(id);
-                if (actor == null)
-                    return NotFound();
+            if (actor == null)
+                return NotFound();
 
-                return View(actor);
-            }
+            return View(actor);
+        }
 
-            public IActionResult Create()
-            {
-                return View();
-            }
+        // GET: Actors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> Create(Actor actor)
-            {
-                if (!ModelState.IsValid)
-                    return View(actor);
+        // POST: Actors/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ActorCreateOrEditViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
 
-                _context.Actors.Add(actor);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
+            var id = await _actorService.CreateAsync(model);
 
-            public async Task<IActionResult> Edit(Guid id)
-            {
-                var actor = await _context.Actors.FindAsync(id);
-                if (actor == null)
-                    return NotFound();
+            return RedirectToAction(nameof(Details), new { id });
+        }
 
-                return View(actor);
-            }
+        // GET: Actors/Edit/{id}
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var actor = await _actorService.GetForEditAsync(id);
 
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> Edit(Actor actor)
-            {
-                if (!ModelState.IsValid)
-                    return View(actor);
+            if (actor == null)
+                return NotFound();
 
-                _context.Actors.Update(actor);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
+            return View(actor);
+        }
 
-            public async Task<IActionResult> Delete(Guid id)
-            {
-                var actor = await _context.Actors.FindAsync(id);
-                if (actor == null)
-                    return NotFound();
+        // POST: Actors/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, ActorCreateOrEditViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
 
-                _context.Actors.Remove(actor);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
+            var updated = await _actorService.UpdateAsync(id, model);
+
+            if (!updated)
+                return NotFound();
+
+            return RedirectToAction(nameof(Details), new { id });
+        }
+
+        // POST: Actors/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var deleted = await _actorService.DeleteAsync(id);
+
+            if (!deleted)
+                return NotFound();
+
+            return RedirectToAction(nameof(Index));
         }
     }
-
-
+}
