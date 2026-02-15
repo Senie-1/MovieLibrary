1956bd362709dd254f24cb18a353091a387bf4d5
viki-417 <vuctoriaa738@gmail.com>
2026-02-15 11:45:13 +0200

    reorganize project structure

---
+++ MovieLibrary.Models/Domain/Entities/Genre.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieLibrary.Models.Domain.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public ICollection<MovieGenre> MovieGenres { get; set; } = new List<MovieGenre>();
+    }
+
+}
---
+++ MovieLibrary.Models/Domain/Entities/UserMovie.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MovieLibrary.Models.Domain.Entities;
+using MovieLibrary.Models.Domain.Enums;
+
+namespace MovieLibrary.Models
+{
+    public class UserMovie
+    {
+        public int Id { get; set; }
+
+    
+        public string UserId { get; set; }= null!;
+        public IdentityUser User { get; set; } = null!;
+
+        public WatchStatus WatchStatus { get; set; }
+
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!;
+
+       
+    }
+
+}
