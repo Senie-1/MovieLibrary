e0bc90dcb0e6eaae3b0139355dde05236890434b
viki-417 <vuctoriaa738@gmail.com>
2026-02-15 12:26:50 +0200

    update actor service

--- MovieLibrary.Business/Services/Interfaces/IActorService.cs
+++ MovieLibrary.Business/Services/Interfaces/IActorService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieLibrary.Models.ViewModels.Actors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace MovieLibrary.Business.Services
 {
-    internal interface IActorService
+    public interface IActorService
     {
+        Task<IEnumerable<ActorViewModel>> GetAllAsync();
+
+        Task<ActorViewModel?> GetByIdAsync(Guid id);
+
+        Task<ActorCreateOrEditViewModel?> GetForEditAsync(Guid id);
+
+        Task<Guid> CreateAsync(ActorCreateOrEditViewModel model);
+
+        Task<bool> UpdateAsync(Guid id, ActorCreateOrEditViewModel model);
+
+        Task<bool> DeleteAsync(Guid id);
+
+        Task<bool> ExistsAsync(Guid id);
     }
 }
