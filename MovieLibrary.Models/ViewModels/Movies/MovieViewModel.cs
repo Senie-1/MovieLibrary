49d00d5f7b5a48c4096447d063df08537abbc954
viki-417 <vuctoriaa738@gmail.com>
2026-02-15 14:59:50 +0200

    add movie viewmodels

---
+++ MovieLibrary.Models/ViewModels/Movies/MovieViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Models.ViewModels.Movies
+{
+    public class MovieViewModel
+    {
+        public Guid Id { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public int ReleaseYear { get; set; }
+
+        public string? PosterUrl { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public List<string> Genres { get; set; } = new();
+
+        public List<string> Actors { get; set; } = new();
+
+        public int ReviewsCount { get; set; }
+
+        public double AverageReviewRating { get; set; }
+    }
+}
