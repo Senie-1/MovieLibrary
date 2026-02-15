76a5fe8bf155b284d46006601c8e2231ec67a9e7
viki-417 <vuctoriaa738@gmail.com>
2026-02-15 15:00:26 +0200

    add movie service interface

---
+++ MovieLibrary.Business/Services/Interfaces/IMovieService.cs
@@ -0,0 +1,20 @@
+﻿using MovieLibrary.Models.ViewModels.Movies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary.Business.Services.Interfaces
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<MovieViewModel>> GetAllAsync();
+        Task<MovieViewModel?> GetByIdAsync(Guid id);
+        Task<MovieCreateOrEditViewModel?> GetForEditAsync(Guid id);
+        Task<Guid> CreateAsync(MovieCreateOrEditViewModel model);
+        Task<bool> UpdateAsync(Guid id, MovieCreateOrEditViewModel model);
+        Task<bool> DeleteAsync(Guid id);
+        Task<bool> ExistsAsync(Guid id);
+    }
+}
